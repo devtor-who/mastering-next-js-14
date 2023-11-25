b1da8c8a3d03568d47d876344aa50d5fee2743d2
dinnertime <dinnertime.dev@gmail.com>
2023-11-25 18:13:17 +0900

    사이트 정보 추가

--- trello-clone/src/app/layout.tsx
+++ trello-clone/src/app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from 'next';
 import { Noto_Sans_KR } from 'next/font/google';
 import './globals.css';
+import { siteConfig } from '@/config/site';
 
 const noto_Sans_KR = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Trelloky - 협업 및 프로젝트 관리',
-  description:
-    '무료 팀 협업 툴로 시작하여 필요에 따라 확장 가능한 프리미엄 플랜을 제공합니다. 실시간 협업과 통합된 리포팅 도구로 프로젝트 관리를 한 단계 업그레이드하세요',
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: [{ href: '/logo.svg', url: '/logo.svg' }],
 };
 
 export default function RootLayout({
